dae3206a58d492acca15f12171db4d51c45d307f
Lysandre <lysandre.debut@reseau.eseo.fr>
2019-12-17 11:36:42 -0500

    Working Rust tokenizer in transformers
    Co-authored-by: Anthony Moi <m.anthony.moi@gmail.com>

--- transformers/tokenization_bert.py
+++ transformers/tokenization_bert.py
@@ -21,7 +21,7 @@ import logging
 import os
 import unicodedata
 from io import open
-from tokenizers import Tokenizer, models, pre_tokenizers, decoders
+from tokenizers import Tokenizer, models, pre_tokenizers, decoders, processors
 
 from .tokenization_utils import PreTrainedTokenizer
 
@@ -280,23 +280,71 @@ class BertTokenizerFast(BertTokenizer):
 
     def __init__(self, vocab_file, do_lower_case=True, do_basic_tokenize=True, never_split=None,
                  unk_token="[UNK]", sep_token="[SEP]", pad_token="[PAD]", cls_token="[CLS]",
-                 mask_token="[MASK]", tokenize_chinese_chars=True, **kwargs):
+                 mask_token="[MASK]", tokenize_chinese_chars=True,
+                 max_length=None,
+                 stride=0,
+                 truncation_strategy='longest_first', **kwargs):
         super(BertTokenizerFast, self).__init__(vocab_file, unk_token=unk_token, sep_token=sep_token,
                                                 pad_token=pad_token, cls_token=cls_token,
                                                 mask_token=mask_token, **kwargs)
-        self.max_len_single_sentence = self.max_len - 2  # take into account special tokens
-        self.max_len_sentences_pair = self.max_len - 3  # take into account special tokens
 
-        if not os.path.isfile(vocab_file):
-            raise ValueError(
-                "Can't find a vocabulary file at path '{}'. To load the vocabulary from a Google pretrained "
-                "model use `tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME)`".format(vocab_file))
-
-        self.tokenizer = Tokenizer(models.WordPiece.from_files(vocab_file))
-        self.tokenizer.with_pre_tokenizer(pre_tokenizers.BasicPreTokenizer.new())
+        self.tokenizer = Tokenizer(models.WordPiece.from_files(
+            vocab_file,
+            unk_token=unk_token
+        ))
+        self.tokenizer.with_pre_tokenizer(pre_tokenizers.BasicPreTokenizer.new(
+            do_lower_case=do_lower_case,
+        ))
         self.tokenizer.with_decoder(decoders.WordPiece.new())
-
+        self.tokenizer.with_post_processor(processors.BertProcessing.new(
+            (sep_token, self.tokenizer.token_to_id(sep_token)),
+            (cls_token, self.tokenizer.token_to_id(cls_token)),
+            max_len=max_length if max_length is not None else self.max_len,
+            trunc_stride=0,
+            trunc_strategy='longest_first',
+        ))
         self.decoder = decoders.WordPiece.new()
+
+    def encode_plus(self,
+                    text,
+                    text_pair=None,
+                    # pad_to_max_length=False,
+                    return_tensors=None,
+                    return_token_type_ids=True,
+                    return_attention_mask=True,
+                    return_overflowing_tokens=False,
+                    return_special_tokens_mask=False,
+                    **kwargs):
+        encoding = self.tokenizer.encode(text, text_pair)
+        encoding_dict = {
+            "input_ids": encoding.ids,
+        }
+
+        if return_token_type_ids:
+            encoding_dict["token_type_ids"] = encoding.type_ids
+
+        if return_attention_mask:
+            encoding_dict["attention_mask"] = encoding.attention_mask
+
+        if return_overflowing_tokens:
+            encoding_dict["overflowing_tokens"] = encoding.overflowing
+
+        if return_special_tokens_mask:
+            encoding_dict["special_tokens_mask"] = encoding.special_tokens_mask
+
+        return encoding_dict
+
+    def add_tokens(self, new_tokens):
+        self.tokenizer.add_tokens(new_tokens)
+
+    def decode(self, tokens):
+        return self.tokenizer.decode(tokens)
+
+    def encode_batch(self, texts):
+        return self.tokenizer.encode_batch(texts)
+
+    def decode_batch(self, ids_batch):
+        return self.tokenizer.decode_batch(ids_batch)
 
     @property
     def vocab_size(self):
@@ -549,3 +597,6 @@ def _is_punctuation(char):
     if cat.startswith("P"):
         return True
     return False
+
+
+
